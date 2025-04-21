4e88d377b05d61605d3ef0ce3a3c6ae5eaf8af60
NNT9x <42613109+nnt9x@users.noreply.github.com>
2025-04-21 11:59:15 +0700

    -up

--- app/Livewire/TableContact.php
+++ app/Livewire/TableContact.php
@@ -5,13 +5,60 @@ namespace App\Livewire;
 use App\Models\Contact;
 use Livewire\Attributes\On;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class TableContact extends Component
 {
+    use WithPagination;
+
+    public $limit = 10;
+    public $search = '';
+
+    # Form update
+    public $contactId;
+    public $name;
+    public $phone;
+    public $email;
+
+    public function deleteContact($contactId)
+    {
+        try {
+            $contact = Contact::query()->findOrFail($contactId);
+            $contact->delete();
+            flash()->success('Xoá thành công!');
+        } catch (\Exception $e) {
+            flash()->error('Xoá thất bại!');
+        }
+    }
+
+    public function updateContact()
+    {
+        try {
+            $contact = Contact::query()->findOrFail($this->contactId);
+            $contact->name = $this->name;
+            $contact->phone = $this->phone;
+            $contact->email = $this->email;
+            $contact->save();
+            flash()->success('Cập nhật thành công!');
+        } catch (\Exception $e) {
+            flash()->error('Cập nhật thất bại!');
+        }
+    }
+
     #[On('contact-created')]
     public function render()
     {
-        $contacts = Contact::query()->get();
+        if (empty($this->search)) {
+            $contacts = Contact::query()->paginate($this->limit);
+        } else {
+            # Reset page
+            $this->resetPage();
+            $contacts = Contact::query()
+                ->where('name', 'like', "%{$this->search}%")
+                ->orWhere('email', 'like', "%{$this->search}%")
+                ->orWhere('phone', 'like', "%{$this->search}%")
+                ->paginate($this->limit);
+        }
         return view('livewire.table-contact', ['contacts' => $contacts]);
     }
 }
--- resources/views/livewire/table-contact.blade.php
+++ resources/views/livewire/table-contact.blade.php
@@ -1,4 +1,29 @@
-<div>
+<div x-data="{contactId: @entangle('contactId'), name: @entangle('name'), phone: @entangle('phone'), email: @entangle('email')}">
+    <div class="flex justify-between mb-3">
+        <select wire:model.live="limit" class="select w-fit">
+            <option value="1">1 bản ghi</option>
+            <option value="5">5 bản ghi</option>
+            <option value="10">10 bản ghi</option>
+            <option value="20">20 bản ghi</option>
+        </select>
+
+        <label class="input">
+            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g
+                    stroke-linejoin="round"
+                    stroke-linecap="round"
+                    stroke-width="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                >
+                    <circle cx="11" cy="11" r="8"></circle>
+                    <path d="m21 21-4.3-4.3"></path>
+                </g>
+            </svg>
+            <input wire:model.live.debounce.500ms="search" type="search" class="grow" placeholder="Search"/>
+        </label>
+    </div>
+
     <table class="table table-zebra border border-gray-200">
         <tr>
             <th>STT</th>
@@ -8,13 +33,40 @@
             <th>Hành động</th>
         </tr>
         @foreach($contacts as $row)
-            <tr>
+            <tr wire:key="{{$row->id}}">
                 <td>{{$loop->iteration}}</td>
                 <td>{{$row->name}}</td>
                 <td>{{$row->phone}}</td>
                 <td>{{$row->email}}</td>
-                <td></td>
+                <td>
+                    <button wire:click="deleteContact('{{$row->id}}')"
+                            wire:confirm="Bạn có chắc chắn muốn xoá liên hệ - {{$row->name}} ?"
+                            class="btn btn-sm btn-soft btn-error">Xoá
+                    </button>
+                    <button x-on:click="contactId='{{$row->id}}', name='{{$row->name}}';phone='{{$row->phone}}';email='{{$row->email}}'; ;editModal.showModal()" class="btn btn-sm btn-soft btn-warning" >Sửa</button>
+                </td>
             </tr>
         @endforeach
     </table>
+    <div>
+        {{$contacts->links()}}
+    </div>
+
+    <dialog id="editModal" class="modal modal-bottom sm:modal-middle">
+        <div class="modal-box">
+            <h3 class="text-lg font-bold my-3">Cập nhật liên hệ</h3>
+            <div>
+                <form wire:submit="updateContact" class="flex flex-col gap-2">
+                    <input x-model="contactId" hidden type="text" class="input w-full"/>
+                    <input x-model="name" type="text" placeholder="Họ tên" class="input w-full"/>
+                    <input x-model="phone" type="text" placeholder="Số điện thoại" class="input w-full"/>
+                    <input x-model="email" type="email" placeholder="Email" class="input w-full"/>
+                    <div class="flex justify-between">
+                        <button x-on:click="editModal.close()" type="button" class="btn btn-soft btn-error">Huỷ</button>
+                        <button x-on:click="editModal.close()" class="btn btn-soft btn-primary w-fit" type="submit">Cập nhật</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </dialog>
 </div>
